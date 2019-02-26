024750100187dc3a687d89ef548e6c8b19d004af
larsvers <larsverspohl@gmail.com>
2019-02-26 17:21:26 +0000

    Step 4

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,31 @@
     <img alt="Vue logo" src="./assets/taco-tree.jpeg">
     <h5>I want Tacos.</h5>
     <hello-world msg="Welcome to Your Taco.js App"/>
+    <taco-graph :dataset="tacodata"></taco-graph>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld';
+import TacoGraph from './components/TacoGraph';
+
+const d3 = Object.assign({}, require('d3-array'), require('d3-fetch'));
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    TacoGraph,
+  },
+  data() {
+    return {
+      tacodata: [],
+    };
+  },
+  created() {
+    d3.csv('./tacodata.csv', d3.autoType).then(res => {
+      this.tacodata = res;
+    });
   },
 };
 </script>
@@ -23,15 +38,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-/* h5 {
-  border: 1px solid tomato;
-} */
-</style>
-
-<style scoped>
-h5 {
-  border: 1px solid tomato;
 }
 </style>
---
+++ src/components/TacoGraph.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div id="graph-wrap">
+      <svg height="110">
+        <g v-for="(row, index) in dataset" :key="index">
+          <rect :y="index * 30" :width="+row.sales" height="20"></rect>
+          <text :y="index * 30" :x="3" dy="0.15em">{{row.product}}</text>
+        </g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TacoGraph',
+  props: {
+    dataset: Array,
+  },
+};
+</script>
+
+<style scoped>
+#graph-wrap {
+  display: inline-block;
+  background: #f5f5a8;
+  padding: 1em;
+}
+
+text {
+  fill: white;
+  text-anchor: start;
+  dominant-baseline: hanging;
+}
+</style>
