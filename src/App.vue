996cc184dddb5f7813bec369ac329909fe82d86c
larsvers <larsverspohl@gmail.com>
2019-02-26 11:43:12 +0000

    Step 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/taco-tree.jpeg">
+    <h5>I want Tacos.</h5>
+    <hello-world msg="Welcome to Your Taco.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* h5 {
+  border: 1px solid tomato;
+} */
+</style>
+
+<style scoped>
+h5 {
+  border: 1px solid tomato;
 }
 </style>
